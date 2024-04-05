a435227928f4b367440134324e6f0c519bef5d8f
lauraT <lauratobon@gmail.com>
2024-04-05 16:23:34 -0500

    trying icons

--- components/atoms/text/index.tsx
+++ components/atoms/text/index.tsx
@@ -1,57 +1,23 @@
 export function Title_1({title} : { title: string} ) {
     return (
-        <h1 style={{
-            font: 'Inter',
-            fontSize: '48px',
-            fontWeight: 1000, 
-            lineHeight: '59.33px',
-            textAlign: 'left',
-            color: '#2B2B2B', 
-        }}>{title}
-        </h1>
-
+        <h1 className="font-inter text-4xl font-extrabold leading-15.33 text-left text-tertiary">{title}</h1>
         )
 }
 
 export function Title_2({title} : { title: string} ) {
     return (
-        <h2 style={{
-            font: 'Inter',
-            fontSize: '16px',
-            fontWeight: 400,
-            lineHeight: '24px',
-            textAlign: 'left',
-            color: '#767676',
-        }}>{title}
-        </h2>
-        )
+    <h2 className="font-inter text-base font-normal leading-6 text-left text-gray-600">{title}</h2>
+    )
 }
 
 export function Subtitle_1({title} : { title: string} ) {
     return (
-        <h3 style={{
-            font: 'Inter',
-            fontSize: '32px',
-            fontWeight: 700, 
-            lineHeight: '39.55px',
-            textAlign: 'left',
-            color: '#2B2B2B', 
-        }}>{title}
-        </h3>
-
-        )
+        <h3 className="font-inter text-2xl font-bold leading-10 text-left text-gray-700">{title}</h3>
+    )
 }
 
 export function Paragraph({text} : { text: string} ) {
     return (
-        <p style={{
-            font: 'Inter',
-            fontSize: '15px',
-            fontWeight: 400,
-            lineHeight: '24px',
-            textAlign: 'left',
-            color: '#767676',
-        }}>{text}
-        </p>
-        )
+        <p className="font-inter text-sm font-normal leading-6 text-left text-gray-600">{text}</p>
+    )
 }
