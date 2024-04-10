3eb0097549b09f204ecadddca79fff60fcdf011a
lauraT <lauratobon@gmail.com>
2024-04-10 16:22:14 -0500

    creatings links

--- components/atoms/buttoms/index.tsx
+++ components/atoms/buttoms/index.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
+import React from "react";
+import Link from 'next/link';
 
 export function Button() {
-  return (
-    <a href="https://www.instagram.com/moradabiker" target="_blank" rel="noopener noreferrer" className="inline-block bg-primary hover:bg-violet-900 text-white font-bold py-2 px-4 rounded shadow-lg">
-  CONTRÁTAME {'->'}
-</a>
-
+    return (
+    <a
+        href="https://www.instagram.com/moradabiker"
+        target="_blank"
+        rel="noopener noreferrer"
+        className=" tooltip-button inline-block bg-primary hover:bg-violet-900 text-white font-bold py-2 px-4 rounded shadow-lg"
+    >
+        CONTRÁTAME {"->"}
+        <span className="tooltip-text bg-blend-color-burn">Ingresa al perfil y envía un mensaje al DM</span>
+    </a>
   );
 }
 
-
-export function Icon({icon}:{icon:string}) {
+export function Icon({ icon, link_ }: { icon: string, link_: string }) {
     return (
-        <button className='text-black'>
-            <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 22 22">
-	            <path fill="#ffb400"  fill-rule="evenodd" d={icon} clip-rule="evenodd"/>
-            </svg>
+        <button className="text-black">
+            <Link href={link_}>
+                
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="48"
+                        height="48"
+                        viewBox="0 0 22 22"
+                    >
+                        <path fill="#ffb400" fill-rule="evenodd" d={icon} clip-rule="evenodd" />
+                    </svg>
+                
+            </Link> 
         </button>
-    );    
+    );
 }
-
-
-
